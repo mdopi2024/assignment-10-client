ae5aebf66c64e56e544731065afb281eb22cfefc
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-12-04 15:27:22 +0600

    register form done

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,29 +1,50 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 
 const Register = () => {
-    const {registerWithEmal}=useContext(AuthContext)
+    const { registerWithEmal,googleLogIn } = useContext(AuthContext)
     const navigate = useNavigate()
-    const handleForm = e=>{
-      e.preventDefault();
-      
-      const form = e.target;
-      const name = form.name.value;
-      const email = form.email.value;
-      const password = form.password.value;
-      registerWithEmal(email,password)
-      .then(result =>{
-        navigate('/')
-      }).catch(error=>{
-        console.log(error.code)
-      })
+    const [showErr, setShowErr] = useState('')
+    console.log(showErr)
+
+    const handleGoogleLogIn = () => {
+        googleLogIn()
+            .then(result => {
+                navigate('/')
+            }).catch(error => {
+                console.log(error.code)
+            })
+    }
+
+
+
+    const handleForm = e => {
+        e.preventDefault();
+
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const regex = /^(?=.*[A-Z])(?=.*[a-z]).{6,}$/
+        if (!regex.test(password)) {
+            return setShowErr('Password must be at least 6 characters long, with at least one uppercase and one lowercase letter.')
+        }
+        registerWithEmal(email, password)
+            .then(result => {
+                navigate('/')
+            }).catch(error => {
+                console.log(error.code)
+                setShowErr(error.code)
+
+            })
     }
     return (
-        <div className="hero bg-base-200 ">
-            <div className="hero-content flex flex-col md:min-w-[500px] ">
-                <h1 className='text-3xl font-bold md:text-4xl'>Register your acount</h1>
+        <div className="hero  my-10 ">
+            <div className="hero-content flex flex-col md:w-[500px] ">
+                <h1 className='text-3xl text-center font-bold md:text-4xl'>Register your acount</h1>
                 <div className="card bg-base-100 w-full min-w-lg  shrink-0 shadow-2xl">
                     <form onSubmit={handleForm} className="card-body">
 
@@ -32,6 +53,12 @@ const Register = () => {
                                 <span className="label-text">Name</span>
                             </label>
                             <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo-URL</span>
+                            </label>
+                            <input type="text" name='photo' placeholder="Photo-URL" className="input input-bordered" required />
                         </div>
 
                         <div className="form-control">
@@ -47,6 +74,11 @@ const Register = () => {
                             <input type="password" name='password' placeholder="password" className="input input-bordered" required />
 
                         </div>
+                        <div>
+                            {
+                                showErr && <p className='text-center font-semibold text-red-600'>{showErr}</p>
+                            }
+                        </div>
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Register</button>
                         </div>
@@ -55,7 +87,7 @@ const Register = () => {
                         <p className='mb-2'>Allready have an accout <Link to='/login'><span className='text-red-600 font-semibold'>Login ?</span> </Link></p>
                     </div>
                 </div>
-                <div className='flex justify-center items-center gap-3 font-bold bg-white py-2 px-4 rounded-xl'> <FcGoogle className='text-3xl' />Login with Google </div>
+                <div onClick={handleGoogleLogIn} className='flex justify-center items-center gap-3 font-bold bg-gray-100 py-2 px-4 rounded-xl'> <FcGoogle className='text-3xl' />Login with Google </div>
             </div>
 
         </div>
