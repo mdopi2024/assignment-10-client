198076731907354d70143da229d270aebeb8fc8c
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-12-06 15:33:46 +0600

    home page done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -53,38 +53,79 @@ const Home = () => {
             {/* abilable  offers */}
             <div className='my-7'>
                 <h1 className='my-6 text-center text-3xl md:text-4xl font-bold text-blue-700'>See Abailable offers</h1>
-               <div className='grid grid-cols-1 md:grid-cols-3 gap-10 mx-5'>
-               <div className="card  shadow-xl bg-[#D1E7FD]">
-                    <div className="card-body">
-                        <h2 className="card-title">Visa Type : <span className='font-semibold'>Student Visa</span> </h2>
-                        <p className='font-semibold text-gray-500'>Offer: <span className=' text-green-600'>50%</span></p>
-                        <p className='font-semibold text-gray-500'>Eligibility: For the first time apply </p>
-                        <p className='font-semibold text-gray-500'>Validity: Valid until: Dec 31, 2024 </p>
-                        <p className='text-gray-500'>If your are interested form higher study in your dream city.As soon as posible apply for visa with 50% discout.Dont miss this offer</p>
-                       
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-10 mx-5'>
+                    <div className="card  shadow-xl bg-[#D1E7FD]">
+                        <div className="card-body">
+                            <h2 className="card-title">Visa Type : <span className='font-semibold'>Student Visa</span> </h2>
+                            <p className='font-semibold text-gray-500'>Offer: <span className=' text-green-600'>50%</span></p>
+                            <p className='font-semibold text-gray-500'>Eligibility: For the first time apply </p>
+                            <p className='font-semibold text-gray-500'>Validity: Valid until: Dec 31, 2024 </p>
+                            <p className='text-gray-500'>If your are interested form higher study in your dream city.As soon as posible apply for visa with 50% discout.Dont miss this offer</p>
+
+                        </div>
+                    </div>
+                    <div className="card  shadow-xl bg-[#D1E7FD]">
+                        <div className="card-body">
+                            <h2 className="card-title">Visa Type : <span className='font-semibold'>Tourist Visa</span> </h2>
+                            <p className='font-semibold text-gray-500'>Offer: <span className=' text-green-600'>40%</span></p>
+                            <p className='font-semibold text-gray-500'>Eligibility: For the first time apply </p>
+                            <p className='font-semibold text-gray-500'>Validity: Valid until: Jan 31, 2025 </p>
+                            <p className='text-gray-500'>If your are interested for explore the world .As soon as posible apply for visa with 40% discout.Dont miss this offer</p>
+
+                        </div>
+                    </div>
+                    <div className="card  shadow-xl bg-[#D1E7FD]">
+                        <div className="card-body">
+                            <h2 className="card-title">Visa Type : <span className='font-semibold'>Official Visa</span> </h2>
+                            <p className='font-semibold text-gray-500'>Offer: <span className=' text-green-600'>30%</span></p>
+                            <p className='font-semibold text-gray-500'>Eligibility: For the first time apply </p>
+                            <p className='font-semibold text-gray-500'>Validity: Valid until: Feb 31, 2025 </p>
+                            <p className='text-gray-500'>If your are worried about your career.As soon as posible apply for visa with 30% discout.Dont miss this offer</p>
+
+                        </div>
                     </div>
                 </div>
-                <div className="card  shadow-xl bg-[#D1E7FD]">
-                    <div className="card-body">
-                        <h2 className="card-title">Visa Type : <span className='font-semibold'>Tourist Visa</span> </h2>
-                        <p className='font-semibold text-gray-500'>Offer: <span className=' text-green-600'>40%</span></p>
-                        <p className='font-semibold text-gray-500'>Eligibility: For the first time apply </p>
-                        <p className='font-semibold text-gray-500'>Validity: Valid until: Jan 31, 2025 </p>
-                        <p className='text-gray-500'>If your are interested for explore the world .As soon as posible apply for visa with 40% discout.Dont miss this offer</p>
-                       
+            </div>
+            {/* your facilites */}
+            <div className='my-16'>
+                <h1 className='my-6 text-center text-3xl md:text-4xl font-bold text-blue-700'>Our Abailable Facilites</h1>
+
+
+                <div className='grid grid-cols-1 md:grid-cols-3 gap-7 mx-5'>
+
+                    <div className="card  shadow-xl bg-[#D1E7FD]">
+                        <div className="card-body">
+                            <h2 className="card-title">Free Consultation </h2>
+                            <p className=' text-gray-500'>Access a detailed checklist of all the documents needed for your visa application to avoid any mistakes</p>
+                            <p className='font-semibold text-gray-500'>Eligibility:  Available for all visa types. </p>
+                            <p className='font-semibold text-gray-500'>Pricing : Free </p>
+                            <p className='font-semibold text-gray-500'>Availability:  Available for 24/7 </p>
+                        </div>
                     </div>
-                </div>
-                <div className="card  shadow-xl bg-[#D1E7FD]">
-                    <div className="card-body">
-                        <h2 className="card-title">Visa Type : <span className='font-semibold'>Official Visa</span> </h2>
-                        <p className='font-semibold text-gray-500'>Offer: <span className=' text-green-600'>30%</span></p>
-                        <p className='font-semibold text-gray-500'>Eligibility: For the first time apply </p>
-                        <p className='font-semibold text-gray-500'>Validity: Valid until: Feb 31, 2025 </p>
-                        <p className='text-gray-500'>If your are worried about your career.As soon as posible apply for visa with 30% discout.Dont miss this offer</p>
-                       
+
+                    <div className="card  shadow-xl bg-[#D1E7FD]">
+                        <div className="card-body">
+                            <h2 className="card-title">Express Visa Processing</h2>
+                            <p className=' text-gray-500'>Opt for faster processing and get your visa approved in half the usual time.</p>
+                            <p className='font-semibold text-gray-500'>Eligibility:  Available for selected visa types (Tourist, Business). </p>
+                            <p className='font-semibold text-gray-500'>Pricing : $20 additional fee </p>
+                            <p className='font-semibold text-gray-500'>Availability: Available all year round </p>
+                        </div>
                     </div>
+
+
+                    <div className="card  shadow-xl bg-[#D1E7FD]">
+                        <div className="card-body">
+                            <h2 className="card-title">Document Checklist</h2>
+                            <p className=' text-gray-500'>Access a detailed checklist of all the documents needed for your visa application to avoid any mistakes.</p>
+                            <p className='font-semibold text-gray-500'>Eligibility:  Available for selected visa types.  </p>
+                            <p className='font-semibold text-gray-500'>Pricing : $20 additional fee </p>
+                            <p className='font-semibold text-gray-500'>Availability:Available 24/7 </p>
+                        </div>
+                    </div>
+
+
                 </div>
-               </div>
             </div>
         </div>
     );
