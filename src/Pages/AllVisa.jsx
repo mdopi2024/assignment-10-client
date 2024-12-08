b4857023c919a93237ccb8190acb373663b686d1
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-12-08 09:13:02 +0600

    all visa search feild done

--- src/Pages/AllVisa.jsx
+++ src/Pages/AllVisa.jsx
@@ -4,16 +4,36 @@ import Visacard from '../components/Visacard';
 
 const AllVisa = () => {
     const loadedVisa = useLoaderData()
-    const [visas,setVisas]=useState(loadedVisa)
+    const [visas, setVisas] = useState(loadedVisa)
+
+    const handleSerch=serch=>{
+       fetch(`http://localhost:5000/visas?search=${serch}`)
+       .then(res=>res.json())
+       .then(data=>{
+       if(data){
+        setVisas(data)
+       }
+       })
+    }
 
     return (
         <div>
+            <div className='flex justify-end items-center'>
+                <div className="dropdown mr-10 my-2">
+                    <div tabIndex={0} role="button" className="btn m-1 border border-[#007BFF] hover:bg-[#0056B3]">Serach by Vist Type</div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <li className='bg-[#007BFF] hover:bg-[#0056B3] mt-3 rounded-md' onClick={()=>handleSerch('Student Visa')}>Student Visa</li>
+                        <li className='bg-[#007BFF] hover:bg-[#0056B3] mt-3 rounded-md' onClick={()=>handleSerch('Tourist Visa')}>Tourist Visa</li>
+                        <li className='bg-[#007BFF] hover:bg-[#0056B3] mt-3 rounded-md' onClick={()=>handleSerch('Official Visa')}>Official Visa</li>
+                    </ul>
+                </div> 
+            </div>
             <h1 className='text-center text-3xl md:text-4xl font-bold my-10'>See all visas</h1>
-           <div className='grid grid-cols-1 md:grid-cols-4 gap-7 px-6 pb-16 '>
-            {
-                visas.map(visa=><Visacard key={visa._id} visa={visa}></Visacard>)
-            }
-           </div>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-7 px-6 pb-16 '>
+                {
+                    visas.map(visa => <Visacard key={visa._id} visa={visa}></Visacard>)
+                }
+            </div>
         </div>
     );
 };
