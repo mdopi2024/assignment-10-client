0e45889f580adb7069487f960dfd290468dc99bf
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2024-12-08 09:38:31 +0600

    my visa application search field done

--- src/Pages/VisaApplication.jsx
+++ src/Pages/VisaApplication.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { data, useLoaderData } from 'react-router-dom';
 import MyAddedVisaCard from '../components/MyAddedVisaCard';
 import MyApplicationVisaCard from '../components/MyApplicationVisaCard';
 
@@ -8,15 +8,35 @@ import MyApplicationVisaCard from '../components/MyApplicationVisaCard';
 const VisaApplication = () => {
 
     const myApplication = useLoaderData()
-    const [application,setApplication]= useState(myApplication)
-    
+    const [application, setApplication] = useState(myApplication)
 
-    
+    const handleSearch = search => {
+       search.preventDefault()
+       const form=search.target;
+       const country = form.country.value
+       fetch(`http://localhost:5000/myvisa?search=${country}`)
+       .then(res=>res.json())
+       .then(data=>{
+        setApplication(data);
+        form.reset()
+       })
+    }
+
     return (
-        <div className='grid grid-cols-1 md:grid-cols-4 gap-8 px-5'>
-        {
-            application.map(card=><MyApplicationVisaCard key={card._id} card={card} application={application} setApplication={setApplication}></MyApplicationVisaCard>)
-        }
+        <div>
+            <div className='flex justify-center items-center mt-7'>
+                <div >
+                    <form onSubmit={handleSearch} className='flex  gap-2'>
+                        <input type="text" name='country' placeholder="Search by country " className="input input-bordered w-full max-w-xs" />
+                        <button className='border rounded-md border-blue-700 hover:bg-blue-800 font-semibold px-5 hover:text-white'>Search</button>
+                    </form>
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-4 gap-8 px-5 my-10'>
+                {
+                    application.map(card => <MyApplicationVisaCard key={card._id} card={card} application={application} setApplication={setApplication}></MyApplicationVisaCard>)
+                }
+            </div>
         </div>
     );
 };
