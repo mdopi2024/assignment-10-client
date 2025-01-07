fdaf456e8986c73e154c485ccc256ceacb59b471
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2025-01-07 11:37:19 +0600

    search done

--- src/Pages/VisaApplication.jsx
+++ src/Pages/VisaApplication.jsx
@@ -1,20 +1,29 @@
 
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { data, useLoaderData } from 'react-router-dom';
 import MyAddedVisaCard from '../components/MyAddedVisaCard';
 import MyApplicationVisaCard from '../components/MyApplicationVisaCard';
+import { AuthContext } from '../AuthProvider/AuthProvider';
 
 
 const VisaApplication = () => {
+    const {user}= useContext(AuthContext)
+    const [application, setApplication] = useState([])
 
-    const myApplication = useLoaderData()
-    const [application, setApplication] = useState(myApplication)
+    useEffect(()=>{
+        fetch(`http://localhost:5000/myvisa/${user?.email}`)
+        .then(res=>res.json())
+        .then(data=>{
+        setApplication(data)
+        })
+    },[])
 
     const handleSearch = search => {
        search.preventDefault()
        const form=search.target;
        const country = form.country.value
-       fetch(`http://localhost:5000/myvisa?search=${country}`)
+       console.log(country)
+       fetch(`http://localhost:5000/myvisa/${user?.email}?search=${country}`)
        .then(res=>res.json())
        .then(data=>{
         setApplication(data);
@@ -24,8 +33,8 @@ const VisaApplication = () => {
 
     return (
         <div>
-            <div className='flex justify-center items-center mt-7'>
-                <div >
+            <div className='flex justify-center items-center mt-7 '>
+                <div className='mt-[100px]' >
                     <form onSubmit={handleSearch} className='flex  gap-2'>
                         <input type="text" name='country' placeholder="Search by country " className="input input-bordered w-full max-w-xs" />
                         <button className='border rounded-md border-blue-700 hover:bg-blue-800 font-semibold px-5 hover:text-white'>Search</button>
@@ -34,7 +43,7 @@ const VisaApplication = () => {
             </div>
             <div className='grid grid-cols-1 md:grid-cols-4 gap-8 px-5 my-10'>
                 {
-                    application.map(card => <MyApplicationVisaCard key={card._id} card={card} application={application} setApplication={setApplication}></MyApplicationVisaCard>)
+                    application?.map(card => <MyApplicationVisaCard key={card._id} card={card} application={application} setApplication={setApplication}></MyApplicationVisaCard>)
                 }
             </div>
         </div>
